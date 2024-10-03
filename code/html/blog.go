e55d13c5c37e42ca898f41a1bad079af0c69e86a
Ben Dorton <ben.dorton@smarty.com>
2024-10-03 11:29:30 -0600

    Blog title

--- code/html/blog.go
+++ code/html/blog.go
@@ -1,6 +1,6 @@
 package html
 
 const (
-	BlogName        = "Name Here"
-	BlogDescription = "A sentence about yourself and/or this blog here."
+	BlogName        = "Monetized Blog"
+	BlogDescription = "A blog by 5 people. Made to be monetized."
 )
