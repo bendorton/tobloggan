4586147b32dbd5a5fb10169419dc73d66b1a811a
Easton Gibson <easton@smarty.com>
2024-10-03 14:33:01 -0600

    Add ArticleParser.

--- code/stations/02_article_parser.go
+++ code/stations/02_article_parser.go
@@ -1,11 +1,44 @@
 package stations
 
-//type ArticleParser struct{}
+import (
+	"encoding/json"
+	"errors"
+	"strings"
 
-//func (this *ArticleParser) Do(input any, output func(any)) {
-//    TODO: given a contracts.SourceFile, parse the JSON metadata and save the body on a contracts.Article.
-//    input: contracts.SourceFile
-//    input: contracts.Article
-//}
+	"tobloggan/code/contracts"
+)
 
-//var errMalformedContent = errors.New("malformed content")
+var (
+	separatorMissing = errors.New("invalid separator")
+)
+
+type ArticleParser struct{}
+
+func NewArticleParser() contracts.Station {
+	return &ArticleParser{}
+}
+
+func (this *ArticleParser) Do(input any, output func(any)) {
+	switch fileContents := input.(type) {
+	case contracts.SourceFile:
+		var article contracts.Article
+		parts := strings.Split(string(fileContents), "+++")
+		if len(parts) != 2 {
+			output(contracts.Errorf("%w: %w", errMalformedContent, separatorMissing))
+			return
+		}
+		header := []byte(strings.TrimSpace(parts[0]))
+		body := parts[1]
+		err := json.Unmarshal(header, &article)
+		if err != nil {
+			output(contracts.Errorf("%w: %w", errMalformedContent, err))
+			return
+		}
+		article.Body = body
+		output(article)
+	default:
+		output(input)
+	}
+}
+
+var errMalformedContent = errors.New("malformed content")
--- code/stations/02_article_parser_test.go
+++ code/stations/02_article_parser_test.go
@@ -1,5 +1,15 @@
 package stations
 
+import (
+	"testing"
+	"time"
+
+	"tobloggan/code/contracts"
+
+	"github.com/smarty/assertions/should"
+	"github.com/smarty/gunit"
+)
+
 //func (this *ArticleParserFixture) TestArticleMetaAndContentReadFromDiskAndEmitted() {}
 //func (this *ArticleParserFixture) TestMissingDivider() {}
 //func (this *ArticleParserFixture) TestMalformedMetadata() {}
@@ -13,3 +23,42 @@ const article1Content = `{
 +++
 
 The contents of article 1.`
+
+func TestArticleParserFixture(t *testing.T) {
+	gunit.Run(new(ArticleParserFixture), t)
+}
+
+type ArticleParserFixture struct {
+	StationFixture
+}
+
+func (this *ArticleParserFixture) Setup() {
+	this.station = NewArticleParser()
+}
+
+func (this *ArticleParserFixture) TestCorrectlyParsingArticle() {
+	t, _ := time.Parse(time.RFC3339, "2024-09-04T00:00:00Z")
+	want := contracts.Article{
+		Draft: false,
+		Slug:  "/article/1",
+		Title: "Article 1",
+		Date:  t,
+		Body:  "\n\nThe contents of article 1.",
+	}
+	this.do(contracts.SourceFile(article1Content))
+	this.assertOutputs(want)
+}
+
+func (this *ArticleParserFixture) TestBadJson() {
+	badJson := "{\"hi\":}+++"
+	this.do(contracts.SourceFile(badJson))
+	this.So(this.outputs, should.HaveLength, 1)
+	this.So(this.outputs[0], should.Wrap, errMalformedContent)
+}
+
+func (this *ArticleParserFixture) TestNoSeparator() {
+	badArticle := "{} ++"
+	this.do(contracts.SourceFile(badArticle))
+	this.So(this.outputs, should.HaveLength, 1)
+	this.So(this.outputs[0], should.Wrap, separatorMissing)
+}
