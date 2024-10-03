3018df74526cbde18070388ad90e6e185aa4e20c
Easton Gibson <easton@smarty.com>
2024-10-03 15:40:45 -0600

    Add ArticleRenderer.

--- code/stations/08_article_renderer.go
+++ code/stations/08_article_renderer.go
@@ -1,15 +1,48 @@
 package stations
 
-//type ArticleRenderer struct {}
-//
-//func (this *ArticleRenderer) Do(input any, output func(any)) {
-//    TODO: combine the fields of the incoming contracts.Article with the article template (provided via the constructor),
-//    replace: {{Title}} with contracts.Article.Title
-//             {{Slug}} with contracts.Article.Slug
-//             {{Date}} with contracts.Article.Date.Format("January 2, 2006")
-//             {{Body}} with contracts.Article.Body
-//    output(contracts.Page{
-//        Path:    input.Slug,
-//        Content: replacedTemplate,
-//    })
-//}
+import (
+	"strings"
+
+	"tobloggan/code/contracts"
+)
+
+type ArticleRenderer struct {
+	template string
+}
+
+func NewArticleRenderer(template string) contracts.Station {
+	return &ArticleRenderer{template: template}
+}
+
+const (
+	titlePlaceholder = "{{Title}}"
+	slugPlaceholder  = "{{Slug}}"
+	datePlaceholder  = "{{Date}}"
+	bodyPlaceholder  = "{{Body}}"
+)
+
+func (this *ArticleRenderer) Do(input any, output func(any)) {
+	//TODO: combine the fields of the incoming contracts.Article with the article template (provided via the constructor),
+	//replace: {{Title}} with contracts.Article.Title
+	//         {{Slug}} with contracts.Article.Slug
+	//         {{Date}} with contracts.Article.Date.Format("January 2, 2006")
+	//         {{Body}} with contracts.Article.Body
+	//output(contracts.Page{
+	//    Path:    input.Slug,
+	//    Content: replacedTemplate,
+	//})
+	switch article := input.(type) {
+	case contracts.Article:
+		this.template = strings.ReplaceAll(this.template, titlePlaceholder, article.Title)
+		this.template = strings.ReplaceAll(this.template, slugPlaceholder, article.Slug)
+		this.template = strings.ReplaceAll(this.template, datePlaceholder, article.Date.Format("January 2, 2006"))
+		this.template = strings.ReplaceAll(this.template, bodyPlaceholder, article.Body)
+		page := contracts.Page{
+			Path:    article.Slug,
+			Content: this.template,
+		}
+		output(page)
+	default:
+		output(input)
+	}
+}
--- code/stations/08_article_renderer_test.go
+++ code/stations/08_article_renderer_test.go
@@ -1,3 +1,61 @@
 package stations
 
-//func (this *ArticleRendererFixture) TestRendering() {}
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	"tobloggan/code/contracts"
+
+	"github.com/smarty/assertions/should"
+	"github.com/smarty/gunit"
+)
+
+func TestArticleRendererFixture(t *testing.T) {
+	gunit.Run(new(ArticleRendererFixture), t)
+}
+
+type ArticleRendererFixture struct {
+	StationFixture
+}
+
+func (this *ArticleRendererFixture) Setup() {
+	this.station = NewArticleRenderer(fmt.Sprintf("%s, %s, %s, %s, %s, %s, %s, %s",
+		titlePlaceholder,
+		bodyPlaceholder,
+		slugPlaceholder,
+		titlePlaceholder,
+		datePlaceholder,
+		titlePlaceholder,
+		datePlaceholder,
+		slugPlaceholder),
+	)
+}
+
+func (this *ArticleRendererFixture) TestRendering() {
+	title := "howdy"
+	body := "Cody wants a better title"
+	now := time.Now()
+	nowStr := now.Format("January 2, 2006")
+	slug := "http"
+	this.do(contracts.Article{
+		Body:  body,
+		Title: title,
+		Date:  now,
+		Slug:  slug,
+	})
+
+	this.So(this.outputs, should.HaveLength, 1)
+	page, ok := this.outputs[0].(contracts.Page)
+	this.So(ok, should.BeTrue)
+	this.So(strings.Contains(page.Content, titlePlaceholder), should.BeFalse)
+	this.So(strings.Contains(page.Content, datePlaceholder), should.BeFalse)
+	this.So(strings.Contains(page.Content, slugPlaceholder), should.BeFalse)
+	this.So(strings.Contains(page.Content, bodyPlaceholder), should.BeFalse)
+
+	this.So(strings.Contains(page.Content, body), should.BeTrue)
+	this.So(strings.Contains(page.Content, title), should.BeTrue)
+	this.So(strings.Contains(page.Content, nowStr), should.BeTrue)
+	this.So(strings.Contains(page.Content, slug), should.BeTrue)
+}
